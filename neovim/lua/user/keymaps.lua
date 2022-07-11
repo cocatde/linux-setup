4a68843cc13cd62cdba9dfd04567f94b3800c687
shu.hui <shu.hui@rea-group.com>
2022-07-11 23:47:24 +1000

    add more keymaps

--- neovim/lua/user/keymaps.lua
+++ neovim/lua/user/keymaps.lua
@@ -3,3 +3,42 @@ local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = " "
 
+-- Modes
+--   normal_mode = "n"
+--   insert_mode = "i"
+--   visual_mode = "v"
+--   visual_block_mode = "x"
+--   term_mode = "t"
+--   command_mode = "c"
+
+
+-- Normal
+-- Resize with arrows
+map("n", "<C-Up>", ":resize +2<CR>", opts)
+map("n", "<C-Down>", ":resize -2<CR>", opts)
+map("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+map("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate Buffers
+map("n", "<S-h>", ":bprevious<CR>", opts)
+map("n", "<S-l>", ":bnext<CR>", opts)
+
+-- Insert
+
+
+-- Visual
+-- Stay in indent mode
+map("v", "<", "<gv", opts)
+map("v", ">", ">gv", opts)
+
+-- Move text up and down
+map("v", "<A-j>", ":m .+1<CR>==", opts)
+map("v", "<A-k>", ":m .-2<CR>==", opts)
+map("v", "p", '"_dP', opts)
+
+-- Visual Block --
+-- Move text up and down
+map("x", "J", ":move '>+1<CR>gv-gv", opts)
+map("x", "K", ":move '<-2<CR>gv-gv", opts)
+map("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+map("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
