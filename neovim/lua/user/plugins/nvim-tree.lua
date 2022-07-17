dac4de85ef38896ca76ce56da55b8c83f6ad1de4
shu.hui <shu.hui@rea-group.com>
2022-07-17 21:21:27 +1000

    format lua code

--- neovim/lua/user/plugins/nvim-tree.lua
+++ neovim/lua/user/plugins/nvim-tree.lua
@@ -1,17 +1,17 @@
 require "nvim-tree".setup {
-    diagnostics = {
-        enable = true,
+  diagnostics = {
+    enable = true,
+  },
+  git = {
+    ignore = false,
+  },
+  renderer = {
+    highlight_opened_files = "2",
+    indent_markers = {
+      enable = true
     },
-    git = {
-        ignore = false,
-    },
-    renderer = {
-        highlight_opened_files = "2",
-        indent_markers = {
-            enable = true
-        },
-        group_empty = true
-    }
+    group_empty = true
+  }
 }
 
 vim.cmd [[highlight NvimTreeIndentMarker guifg=#30323E]]
